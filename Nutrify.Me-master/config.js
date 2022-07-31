1f60b0c1c14488ada65dcfb3a7710cbe04f49c2c
thenish251 <94748793+thenish251@users.noreply.github.com>
2022-07-31 14:58:32 +0530

    Delete Nutrify.Me-master directory

--- Nutrify.Me-master/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secret': 'supersecret'
-  };
-
-    module.exports = {
-    'nutritionixEndPoint': 'https://trackapi.nutritionix.com/v2/natural/nutrients/',
-  };
-
-  //module.exports = config;
